d98c368cfdd5677f726330ec817af15415b42b54
Hlxwk <1296835347@qq.com>
2018-09-18 08:52:51 +0800

    2018/9/18

---
+++ ch2/helloSLAM.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"hello SLAM!"<<endl;
+   return 0;
+}
---
+++ ch2/useHello.cpp
@@ -0,0 +1,6 @@
+#include "libHelloSLAM.h"
+int main()
+{
+    printHello();
+    return 0;
+}
